3cb5c408c3cc7c80aba05b02d1e70bc14e8354b2
YasirAnsari45 <mryasir969@gmail.com>
2025-11-21 10:36:54 +0530

    first commit

---
+++ task.py
@@ -0,0 +1,26 @@
+"""a = [3, 1, 3, 2, 2, 4]
+l = []
+for i in a:
+    if i not in l:
+        l.append(i)
+print(l)"""
+
+"""a = [1, 5, 2, 5, 3]
+for i in range(len(a)):
+     if a[i] in a[:i] + a[i+1]:
+        print(a[i])"""
+
+"""a = {
+ "user": {
+ "firstname": "Alice",
+ "lastname": "Brown"
+ },
+ "age": 30
+ }
+
+d = {}
+for i in a:
+    if isinstance(i,dict):"""
+         
+       
+           
